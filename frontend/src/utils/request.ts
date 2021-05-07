fa4ae98633e5825956d07fd1309fb50fab83802b
Jeterson Miranda Gomes <jeterson.gomes@mv.com.br>
2021-05-07 15:26:18 -0300

    Deixando somente variavel na base url

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?  process.env.REACT_APP_BACKEND_URL : 'http//localhost:8080';
-//'https://sds3-jeterson.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL
